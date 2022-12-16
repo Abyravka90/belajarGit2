1b596424c9e47a94d7053b1c457954f31f1dbf17
S.F.R <110540424+SFRMado@users.noreply.github.com>
2022-12-16 22:23:06 +0700

    adding remedial tasks file

---
+++ PHP/no.1.php
@@ -0,0 +1,15 @@
+<?php
+$namalengkap = "SFR";
+$NIK = "5569873652";
+$JenisKelamin = "Laki-Laki";
+$TempatLahir = "Jakarta";
+$TanggalLahir = "23 Desember 2004";
+$Alamat = "Citra Indah City";
+
+echo "Nama Lengkap = " . $namalengkap . "<br />"; 
+echo "NIK = " . $NIK."<br />";
+echo "Jenis Kelamin = " . $JenisKelamin."<br />";
+echo "Tempat Lahir = " . $TempatLahir."<br />";
+echo "Tanggal Lahir = " . $TanggalLahir."<br />";
+echo "Alamat = " . $Alamat."br";
+?>
