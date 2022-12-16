be8492ce92ab009c65a09359b959e5f528594496
Aisyah shania putri <101912222+aisyahsh@users.noreply.github.com>
2022-12-16 13:39:26 +0700

    Remedial Aisyah

---
+++ Remedial_Aisyah/PBO/no1.php
@@ -0,0 +1,5 @@
+<?php
+$nama ="Aisyah"; $NIK = 84729462; $Jenis_kelamin ="Perempuan"; $TempatLahir ="Bogor"; $TanggalLahir ="15 Juni"; $alamat = "Klapanunggal";
+
+    echo "Nama Lengkap: ". $nama." NiK:". $NIK."Jenis Kelamin:" . $Jenis_kelamin." Tempat Lahir: " . $TempatLahir. "Tanggal Lahir: ". $TanggalLahir. "alamat:" . $alamat;
+?>
