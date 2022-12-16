dad1a1b7b1aa7ae9a513d107da08f7812c0fe54a
Ahmad Reza A <areza7095@gmail.com>
2022-12-16 11:01:52 +0700

    mengumpulkan PWPB

---
+++ remedial_reza/PWPB/js_1.js
@@ -0,0 +1,6 @@
+function perjalanan() {
+    var jarak = 677;
+    var kecepatan = 60;
+    var hitung = jarak / kecepatan;
+    return(`lama perjalanan yang ditempuh dengan kecepatan ${kecepatan} km/jam adalah ${hitung} jam}`)
+}
