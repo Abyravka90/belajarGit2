79904b2cd887cfb863070487e811726ad8eca87b
Ahmad Reza A <areza7095@gmail.com>
2022-12-16 11:51:02 +0700

    mengumpulkan PBO

---
+++ remedial_reza/PBO/php_1.php
@@ -0,0 +1,16 @@
+<?php
+$nama = "Ahmad Reza A";
+$nik = "123456789";
+$jenisKelamin = "Laki-Laki";
+$tempatLahir = "Bogor";
+$tanggalLahir = "12-12-2000";
+$alamat = "PPC 5";
+
+echo "Nama : $nama <br>";
+echo "NIK : $nik <br>";
+echo "Jenis Kelamin : $JenisKelamin <br>";
+echo "Tempat Lahir : $tempatLahir <br>";
+echo "Tanggal Lahir : $tanggalLahir <br>";
+echo "Alamat : $alamat <br>";
+
+?>
