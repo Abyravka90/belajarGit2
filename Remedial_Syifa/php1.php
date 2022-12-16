6ad25b070cf73bff3a688183a5823f77822eac44
Syifa <syifaingez200705@gmail.com>
2022-12-16 11:31:57 +0700

    Remedial Syifa

---
+++ Remedial_Syifa/php1.php
@@ -0,0 +1,11 @@
+<?php
+$nama ="Syifa Salsabila Hadi";
+$nik ="15735299";
+$JenisKelamin ="perempuan";
+$TempatLahir ="Jakarta";
+$tanggalLahir ="20 juli 2005";
+$alamat ="graha prima";
+
+echo "nama:".$nama."\n"."nik:".$nik."\n"."jenisKelamin".$JenisKelamin."\n"."TempatLhir:".$TempatLahir."\n".
+"tanggalLahir:".$tanggalLahir."\n"."alamat:".$alamat;
+?>
